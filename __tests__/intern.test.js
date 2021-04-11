13381f0c364ae22331aa1a6d2b85a6e545e16275
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-11 16:04:16 -0500

    changed fictitious name

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -3,18 +3,18 @@ const Employee = require("../lib/employee");
 
 test("Can set school via constructor argument", () => {
     const testValue = "SMU";
-    const e = new Intern("Bob", testValue);
+    const e = new Intern("Ryan Howard", 3, "rhoward@dunder-mifflin.com", testValue);
     expect(e.school).toBe(testValue);
 });
 
 test("getRole() should return \"Intern\"", () => {
     const testValue = "Intern",
-    const e = new Intern("Bob", 3, "SMU");
+    const e = new Intern("Ryan Howard", 3, "rhoward@dunder-mifflin.com", "SMU");
     expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get school from getSchool()", () => {
     const testValue = "SMU";
-    const e = new Intern("Bob", 3, testValue);
+    const e = new Intern("Ryan Howard", 3, "rhoward@dunder-mifflin.com", testValue);
     expect(e.getSchool()).toBe(testValue);
 });
