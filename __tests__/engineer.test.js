5595200394adbdbd4f2f744596c56d63aaac8584
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-11 16:03:25 -0500

    changed fictitious name

---
+++ __tests__/engineer.test.js
@@ -0,0 +1,20 @@
+const Engineer = require("../lib/engineer");
+const Employee = require("../lib/employee");
+
+test("Can set GitHub username via constructor argument", () => {
+    const testValue = "dks1725";
+    const e = new Engineer("Dwight Schrute", 2, "dschrute@dunder-mifflin.com", testValue);
+    expect(e.github).toBe(testValue);
+});
+
+test("getRole() should return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Dwight Schrute", 2, "dschrute@dunder-mifflin.com", "dks1725");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get GitHub username from getGithub()", () => {
+    const testValue = "dks1725";
+    const e = new Engineer("Dwight Schrute", 2, "dschrute@dunder-mifflin.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+});
