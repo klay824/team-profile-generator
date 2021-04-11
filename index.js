18568771f5a9f9bd094be5fe5f90ef232cdbbd94
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-11 18:05:54 -0500

    beginning of inquirer questions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const Handlebars = require("handlebars");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'teamName',
+            message: 'What is your team name?',
+            validate: function(response) {
+                if(response.length < 1 ) {
+                    return console.log("Please enter a team name to continue.");
+                }
+                return true;
+            }
+        },
+        {
+            
+        }
+    ])
+}
