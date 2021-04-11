73d05ed124bc8a705c4694dde091fc7a1ddec126
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-11 15:34:40 -0500

    created intern test

---
+++ __tests__/intern.test.js
@@ -0,0 +1,20 @@
+const Intern = require("../lib/intern");
+const Employee = require("../lib/employee");
+
+test("Can set school via constructor argument", () => {
+    const testValue = "SMU";
+    const e = new Intern("Bob", testValue);
+    expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern",
+    const e = new Intern("Bob", 3, "SMU");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get school from getSchool()", () => {
+    const testValue = "SMU";
+    const e = new Intern("Bob", 3, testValue);
+    expect(e.getSchool()).toBe(testValue);
+})
