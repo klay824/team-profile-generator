2b823fe3754304542dcc9e5f4574c837524d6358
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-11 20:33:01 -0500

    began writing questions

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-const Handlebars = require("handlebars");
+// const Handlebars = require("handlebars");
 const inquirer = require('inquirer');
 const fs = require('fs');
-const util = require('util');
 
-const writeFileAsync = util.promisify(fs.writeFile);
+const Employee = require('../lib/employee');
+const Manager = require('../lib/manager');
+const Engineer = require('../lib/engineer');
+const Intern = require('../lib/intern');
 
-const promptUser = () => {
-    return inquirer.prompt([
+inquirer
+    .prompt([
         {
             type: 'input',
             name: 'teamName',
-            message: 'What is your team name?',
-            validate: function(response) {
-                if(response.length < 1 ) {
-                    return console.log("Please enter a team name to continue.");
-                }
-                return true;
-            }
+            message: "Please enter your team's name."
         },
         {
-            
-        }
+            type: 'input',
+            name: 'manager',
+            message: "Please enter the team manager's name."
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the team manager's employee ID."
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the team manager's email address."
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "Please enter the team manager's office number."
+        },
     ])
-}
+    .then((answers) => {
+        const htmlPageContent = generateHTML(answers);
+        fs.writeFile('./dist/index.html', htmlPageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created index.html in the dist folder!')
+        );
+    });
