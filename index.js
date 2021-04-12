8a26f8a658e829c3544b4e869d3d3ad5e07c3bd4
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-11 21:30:08 -0500

    adding more questions

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const Employee = require('../lib/employee');
-const Manager = require('../lib/manager');
-const Engineer = require('../lib/engineer');
-const Intern = require('../lib/intern');
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
 
 inquirer
     .prompt([
@@ -34,10 +34,100 @@ inquirer
             name: 'officeNumber',
             message: "Please enter the team manager's office number."
         },
+        {
+            type: 'confirm',
+            name: 'nextEmployee',
+            message: "Do you want to add more employees?",
+        },
     ])
     .then((answers) => {
-        const htmlPageContent = generateHTML(answers);
-        fs.writeFile('./dist/index.html', htmlPageContent, (err) =>
-            err ? console.log(err) : console.log('Successfully created index.html in the dist folder!')
-        );
-    });
+        if (answers.nextEmployee) {
+            inquirer
+                .prompt([
+                    {
+                        type: 'list',
+                        name: 'addNew',
+                        choices: ['Engineer', "Intern"]
+                    },
+                ])
+                .then((answers) => {
+                    if (answers.addNew === "Engineer") {
+                        inquirer
+                            .prompt([
+                                {
+                                    type: 'input',
+                                    name: 'engineer',
+                                    message: "Please enter the engineer's name.",
+                                },
+                                {
+                                    type: 'input',
+                                    name: 'id',
+                                    message: "Please enter the engineer's employee ID."
+                                },
+                                {
+                                    type: 'input',
+                                    name: 'email',
+                                    message: "Please enter the engineer's email address.",
+                                },
+                                {
+                                    type: 'input',
+                                    name: 'GitHub',
+                                    message: "Please enter the Engineer's GitHub username.",
+                                },
+                                {
+                                    type: 'confirm',
+                                    name: 'nextEmployee',
+                                    message: "Do you want to add more employees?",
+                                },
+                            ])
+                            .then((answers) => {
+                                const htmlPageContent = generateHTML(answers);
+                                fs.writeFile('./dist/index.html', htmlPageContent, (err) =>
+                                    err ? console.log(err) : console.log('Successfully created index.html in the dist folder!')
+                                );
+                            })
+                    } else if (answers.addNew === "Intern") {
+                        inquirer
+                            .prompt([
+                                {
+                                    type: 'input',
+                                    name: 'intern',
+                                    message: "Please enter the interns's name.",
+                                },
+                                {
+                                    type: 'input',
+                                    name: 'id',
+                                    message: "Please enter the interns's employee ID."
+                                },
+                                {
+                                    type: 'input',
+                                    name: 'email',
+                                    message: "Please enter the interns's email address.",
+                                },
+                                {
+                                    type: 'input',
+                                    name: 'GitHub',
+                                    message: "Please enter the intern's school.",
+                                },
+                            ])
+                    }
+                })
+            // } else (answers.confirm_answer) {
+            //   // the user definitely doesn't want pizza
+            // } 
+        } else {
+            const htmlPageContent = generateHTML(answers);
+            fs.writeFile('./dist/index.html', htmlPageContent, (err) =>
+                err ? console.log(err) : console.log('Successfully created index.html in the dist folder!')
+            );
+
+        }
+    })
+
+
+
+
+
+
+
+
