8cef5e0225feddb945bf568e881440446eb7e63f
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-13 21:21:24 -0500

    changed bootstrap classes and added email and github links

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -9,6 +9,8 @@ let header = `
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
     integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Tangerine">
     <link rel="stylesheet" type="text/css" href="./style.css">
     <title>My Team</title>
 </head>
@@ -16,15 +18,17 @@ let header = `
 <body>
     <div class="jumbotron jumbotron-fluid">
         <div class="container text-center">
-            <h1 class="display-4">My Team</h1>            
+            <h1 class="display-4">MY TEAM</h1>            
         </div>
     </div>
 
     <!-- Team card will go below here -->
-    <main class="container">`;
+    <main class="container">
+        <div class="row justify-content-center">`;
 
 
 let footer = `
+        </div>
     </main>
 
 </body>
@@ -32,47 +36,53 @@ let footer = `
 </html>`
 
 function generateHTML(team) {
+    // Manager
     for (i = 0; i < team.length; i++) {
         if (team[i].getRole() == "Manager") {
             header += `
-            <div class="card m-2 col-lg-4" style="width: 18rem;">
-                <div class="card-header">
-                <i class="fas fa-mug-hot"></i> ${team[i].name}
+            <div class="card m-3 col-lg-4" style="width: 18rem;">
+                <div class="card-header mt-3 mb-3">
+                    <h3>${team[i].name}</h3>
+                    <h4><i class="fas fa-mug-hot"></i> Manager</h4>
                 </div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item">${team[i].id}</li>
-                <li class="list-group-item">${team[i].email}</li>
-                <li class="list-group-item">${team[i].officeNumber}</li>
+                <ul class="list-group list-group-flush mb-3">
+                <li class="list-group-item">Employee ID: ${team[i].id}</li>
+                <li class="list-group-item">Email: <a href=mailto:${team[i].email} target="_blank">${team[i].email}</a>
+                <li class="list-group-item">Office number: ${team[i].officeNumber}</li>
                 </ul>
             </div>`
         }
     }
+    // Engineer
     for (i = 0; i < team.length; i++) {
         if (team[i].getRole() == "Engineer") {
             header += `
-            <div class="card m-2 col-lg-4" style="width: 18rem;">
-                <div class="card-header">
-                <i class="fas fa-glasses"></i> ${team[i].name}
+            <div class="card m-3 col-lg-4" style="width: 18rem;">
+                <div class="card-header mt-3 mb-3">
+                    <h3>${team[i].name}</h3>
+                    <h4><i class="fas fa-glasses"></i> Engineer</h4>
                 </div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item">${team[i].id}</li>
-                <li class="list-group-item">${team[i].email}</li>
-                <li class="list-group-item">${team[i].GitHub}</li>
+                <ul class="list-group list-group-flush mb-3">
+                <li class="list-group-item">Employee ID: ${team[i].id}</li>
+                <li class="list-group-item">Email: <a href=mailto:${team[i].email} target="_blank">${team[i].email}</a>
+                <li class="list-group-item">GitHub: <a href="https://github.com/${team[i].GitHub}" target="_blank">${team[i].GitHub}</a></li>
                 </ul>
             </div>`
         }
     }
+    // Intern
     for (i = 0; i < team.length; i++) {
         if (team[i].getRole() == "Intern") {
             header += `
-            <div class="card m-2 col-lg-4" style="width: 18rem;">
-                <div class="card-header">
-                <i class="fas fa-user-graduate"></i> ${team[i].name}
+            <div class="card m-3 col-lg-4" style="width: 18rem;">
+                <div class="card-header mt-3 mb-3">
+                    <h3>${team[i].name}</h3>
+                    <h4><i class="fas fa-user-graduate"></i> Intern</h4>
                 </div>
-                <ul class="list-group list-group-flush">
-                <li class="list-group-item">${team[i].id}</li>
-                <li class="list-group-item">${team[i].email}</li>
-                <li class="list-group-item">${team[i].school}</li>
+                <ul class="list-group list-group-flush mb-3">
+                <li class="list-group-item">Employee ID: ${team[i].id}</li>
+                <li class="list-group-item">Email: <a href=mailto:${team[i].email} target="_blank">${team[i].email}</a></li>
+                <li class="list-group-item">School: ${team[i].school}</li>
                 </ul>
             </div>`
         }
